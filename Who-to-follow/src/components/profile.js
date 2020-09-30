a90daf49b00ab840e4dcc442668b96e0229c6f37
Andrew Joy <56839275+AndrewJoyT@users.noreply.github.com>
2020-09-30 13:39:31 -0400

    Update profile.js

--- Who-to-follow/src/components/profile.js
+++ Who-to-follow/src/components/profile.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 function Profile() {
   const [profile, setProfile] = useState([]);
   const { userId } = useParams();
 
-  const getDetail = () => { 
+  const getDetail = () => {
     axios
       .get(`https://api.github.com/users/${userId}`)
       .then((response) => setProfile(response.data))
       .catch((error) => console.log(error));
-  }
+  };
 
   useEffect(() => {
     getDetail();
@@ -20,17 +20,33 @@ function Profile() {
   return (
     <div className="App">
       <div className="title">
-        <h1>Profile</h1> 
+        <h1>Profile</h1>
       </div>
       <div className="users">
-        <img className="float-left rounded avatar" src={profile.avatar_url} alt={profile.name}/>
+        <img
+          className="float-left rounded avatar"
+          src={profile.avatar_url}
+          alt={profile.name}
+        />
         <div className="d-inline-block">
-            <div>Name:  <span className="name">{profile.name}</span></div> 
-            <div>Username: <span className="name">@{profile.login}</span></div>
-            <div>Email: <span className="name">{profile.email}</span></div>
-            <div>Location: <span className="name">{profile.location}</span></div>
-            <div>Bio: <span className="name">{profile.bio}</span></div>
-            <div>Blog: <span className="name">{profile.blog}</span></div>
+          <div>
+            Name: <span className="name">{profile.name}</span>
+          </div>
+          <div>
+            Username: <span className="name">@{profile.login}</span>
+          </div>
+          <div>
+            Email: <span className="name">{profile.email}</span>
+          </div>
+          <div>
+            Location: <span className="name">{profile.location}</span>
+          </div>
+          <div>
+            Bio: <span className="name">{profile.bio}</span>
+          </div>
+          <div>
+            Blog: <span className="name">{profile.blog}</span>
+          </div>
         </div>
       </div>
       <Link to="/">Back</Link>
